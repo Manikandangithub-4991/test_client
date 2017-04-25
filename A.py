d377e394de8a6cbcb231c72d7744ded3356cddb5
Manikandangithub-4991 <greatmani1994@gmail.com>
2017-04-25 21:57:49 +0530

    added A

---
+++ A.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class A(unittest.TestCase):
+    def test_valid(self):
+        self.assertEqual(1,1)
+    def test_valid1(self):
+        self.assertEqual(2,2)
+
+    @staticmethod
+    def suite():
+        suite = unittest.TestLoader().loadTestsFromTestCase(A)
+        unittest.TextTestRunner(verbosity=2).run(suite)
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(A)
+    unittest.TextTestRunner(verbosity=2).run(suite)
